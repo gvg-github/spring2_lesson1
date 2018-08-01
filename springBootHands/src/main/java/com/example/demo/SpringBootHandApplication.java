fca361a0efe833e50fb22cd8ef47e56823ed5710
gvg-job4j <gvgjava2018@yandex.ru>
2018-08-01 23:40:47 +0300

    Initial commit

---
+++ springBootHands/src/main/java/com/example/demo/SpringBootHandApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author Valeriy Gyrievskikh
+ * @since 01.08.2018.
+ */
+@SpringBootApplication
+public class SpringBootHandApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHandApplication.class, args);
+
+    }
+}
